f7e61702b572b5fec35275f551d6b6e8546ecbfe
Guhello <gk17638292861@163.com>
2019-05-28 14:30:41 +0800

    change new info

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -3,8 +3,8 @@ package com.qfedu.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("hahaha");
-        System.out.println("error");
-        System.out.println("mememe");
+        //System.out.println("error");
+        //System.out.println("mememe");
         int a = 10;
     }
 }
