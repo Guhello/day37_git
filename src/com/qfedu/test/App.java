ba9070bee3e043f794a53fae59a07ad6e8a27c49
Guhello <51065650+Guhello@users.noreply.github.com>
2019-05-28 11:42:18 +0800

    Update App.java

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -3,6 +3,6 @@ package com.qfedu.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("hahaha");
-        System.out.println("heiheihei");
+        //System.out.println("heiheihei");
     }
 }
