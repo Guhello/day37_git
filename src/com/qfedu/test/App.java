6e7679d6482847b5a709d54e8bccc416e1002512
Guhello <51065650+Guhello@users.noreply.github.com>
2019-05-28 11:47:12 +0800

    change

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -3,7 +3,7 @@ package com.qfedu.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("hahaha");
-        //System.out.println("heiheihei");
-        
+        System.out.println("error");
+        System.out.println("mememe");
     }
 }
