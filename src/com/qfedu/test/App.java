3454fe26cdce963490b5fda1ef1eacce85a1c7dd
Guhello <gk17638292861@163.com>
2019-05-28 11:48:46 +0800

    change new info

--- src/com/qfedu/test/App.java
+++ src/com/qfedu/test/App.java
@@ -2,8 +2,8 @@ package com.qfedu.test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("hahaha");
-        //System.out.println("heiheihei");
-        
+        System.out.println("meme");
+        //System.out.println("eeee");
+        int a = 10;
     }
 }
